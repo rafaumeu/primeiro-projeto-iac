d344e011234f7970dfeaf31fd597c20c82a844cc
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-02-04 20:25:32 -0300

    feat(infra): adds support for multiple environments in S3 bucket
    - Updates S3 bucket name to include environment name (terraform.workspace)
    - Adds context tag to S3 bucket with environment name

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rafael-bucket-iac"
+  bucket = "rafael-bucket-iac-${terraform.workspace}"
   tags = {
     Name = "Primeiro bucket criado com terraform"
     Iac = true
+    context = "${terraform.workspace}"
   }
 }
